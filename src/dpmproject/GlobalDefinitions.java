841041e058b217d5bc854aeac4a1a722eac83880
Ali <ali.sharif2@mail.mcgill.ca>
2017-03-11 00:09:01 -0500

    Added motor and sensor intialization

--- src/dpmproject/GlobalDefinitions.java
+++ src/dpmproject/GlobalDefinitions.java
@@ -3,9 +3,11 @@ package dpmproject;
  * 
  */
 
+import lejos.hardware.ev3.LocalEV3;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.motor.UnregulatedMotor;
-import lejos.robotics.SampleProvider;
+import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
 
 /**
  * @author Ali
@@ -23,15 +25,17 @@ public final class GlobalDefinitions {
   public static final double TURN_SPEED                       = 50.0; // In degrees per second
 
   public static final int SWING_TIME                          = 500;  // In milliseconds
+  
+  private static final String leftMotorPort                   = "A";
+  private static final String rightMotorPort                  = "D";
+  private static final String launcherMotorPort               = "C";
+  private static final String lightSensorPort                 = "1";
+  private static final String usSensorPort                    = "3";
 
-
-  // TODO fill in sensor and motor initialization
-  /*
-  public static final EV3LargeRegulatedMotor LEFT_MOTOR;
-  public static final EV3LargeRegulatedMotor RIGHT_MOTOR;
-  public static final UnregulatedMotor LAUNCHER_MOTOR;
-  public static final SampleProvider LIGHT_SENSOR;
-  public static final SampleProvider US_SENSOR;
-  */
+  public static final EV3LargeRegulatedMotor LEFT_MOTOR       = new EV3LargeRegulatedMotor(LocalEV3.get().getPort(leftMotorPort));
+  public static final EV3LargeRegulatedMotor RIGHT_MOTOR      = new EV3LargeRegulatedMotor(LocalEV3.get().getPort(rightMotorPort));
+  public static final UnregulatedMotor LAUNCHER_MOTOR         = new UnregulatedMotor(LocalEV3.get().getPort(launcherMotorPort));
+  public static final EV3ColorSensor LIGHT_SENSOR             = new EV3ColorSensor(LocalEV3.get().getPort(lightSensorPort));
+  public static final EV3UltrasonicSensor US_SENSOR           = new EV3UltrasonicSensor(LocalEV3.get().getPort(usSensorPort));
 
 }
