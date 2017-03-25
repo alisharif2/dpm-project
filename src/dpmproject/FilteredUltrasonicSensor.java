560f25a4cde53590fd3e3302184172e6d08f8eee
Ali <ali.sharif2@mail.mcgill.ca>
2017-03-24 23:21:04 -0400

    Added clipping filter

--- src/dpmproject/FilteredUltrasonicSensor.java
+++ src/dpmproject/FilteredUltrasonicSensor.java
@@ -20,8 +20,10 @@ public class FilteredUltrasonicSensor extends Thread implements SensorInterface 
 	private SampleProvider ultrasonicSampleProvider;
 	private float[] ultrasonicData;
 	private double reading;
+	private int filterControl = 0;
 	
 	private static final int SENSOR_POLL_PERIOD = 10, NUMBER_OF_SAMPLES = 5;
+	private static final int FILTER_OUT = 20;
 	
 	public FilteredUltrasonicSensor(EV3UltrasonicSensor usSensor) {
 		// TODO Auto-generated constructor stub
@@ -62,10 +64,29 @@ public class FilteredUltrasonicSensor extends Thread implements SensorInterface 
 		// TODO Auto-generated method stub
 		// Don't use even numbers
 		double filteredDistance;
+		double distance;
 		ArrayList<Double> samples = new ArrayList<Double>();
 		
 		for(int i = 0;i < NUMBER_OF_SAMPLES;++i) {
-			samples.add(getRawData());
+			distance = getRawData();
+			
+			// I stole this filter from lab1
+			filteredDistance = distance;
+			if (distance >= 255 && filterControl < FILTER_OUT) {
+				// bad value, do not set the distance var, however do increment the
+				// filter value
+				filterControl++;
+			} else if (distance >= 255) {
+				// We have repeated large values, so there must actually be nothing
+				// there: leave the distance alone
+				filteredDistance = distance;
+			} else {
+				// distance went below 255: reset filter and leave
+				// distance alone.
+				filterControl = 0;
+				filteredDistance = distance;
+			}
+			samples.add(filteredDistance);
 			// Use an extremely short delay to make sure we get useful sensor readings
 			Delay.msDelay(SENSOR_POLL_PERIOD);
 		}
