9a82a6dc606ae2b50c305ea7c7f2adaa1099628a
Ali <ali.sharif2@mail.mcgill.ca>
2017-03-26 19:00:13 -0400

    constants loaded from wifi into global definitions in entry point

--- src/dpmproject/EntryPoint.java
+++ src/dpmproject/EntryPoint.java
@@ -6,6 +6,7 @@ package dpmproject;
 import java.util.Map;
 
 import interfacePackages.PathfinderInterface;
+import interfacePackages.SensorInterface;
 import lejos.hardware.Button;
 import lejos.hardware.Sound;
 import lejos.hardware.ev3.LocalEV3;
@@ -29,45 +30,36 @@ public class EntryPoint {
 	 */
 	public static void main(String[] args) {
 		
+		GlobalDefinitions.init();
+		Odometer odo = new Odometer(GlobalDefinitions.LEFT_MOTOR, GlobalDefinitions.RIGHT_MOTOR, 30, true);
+		
 		WifiConnection conn = new WifiConnection("192.168.2.3", 2, false);
-
 		// Connect to server and get the data, catching any errors that might
 		// occur
 		try {
 			Map data = conn.getData();
-
-			GlobalDefinitions.d1 = ((Long) data.get("d1")).intValue();
-			//GlobalDefinitions.FWD_CORNER = ((Long) data.get("FWD_CORNER")).intValue();
-
+			initFromWifi(data);
 		} catch (Exception e) {
 			System.err.println("Error: " + e.getMessage());
 		}
-
 		Sound.beep();
-		
-		GlobalDefinitions.init();
-				
-		Odometer odo = new Odometer(GlobalDefinitions.LEFT_MOTOR, GlobalDefinitions.RIGHT_MOTOR, 30, true);
-		
-		Navigation nav = new Navigation(odo);
-		
-		FilteredColorSensor rearColor = new FilteredColorSensor(GlobalDefinitions.REAR_COLOR_SENSOR);
-		LightLocalizer lsl = new LightLocalizer(odo, rearColor);
-		lsl.doLocalization();
-		
-		float[] data = new float[GlobalDefinitions.US_SENSOR.sampleSize()];
-		FilteredUltrasonicSensor usSensor = new FilteredUltrasonicSensor(GlobalDefinitions.US_SENSOR);
-		RoughUSLocalizer usl = new RoughUSLocalizer(odo, GlobalDefinitions.US_SENSOR.getDistanceMode(), data,RoughUSLocalizer.LocalizationType.FALLING_EDGE, GlobalDefinitions.LEFT_MOTOR, GlobalDefinitions.RIGHT_MOTOR);
+			
+		SensorInterface usSensor = new FilteredUltrasonicSensor(GlobalDefinitions.US_SENSOR);
+		FrameworkUSLocalizer usl = new FrameworkUSLocalizer(odo, usSensor);
 		usl.doLocalization();
-		
 		Sound.beep();
-		
-		nav.travelTo(5 * GlobalDefinitions.TILE_SIZE, GlobalDefinitions.d1 * GlobalDefinitions.TILE_SIZE);
-		nav.turnTo(0, true);
-		
-		DemoBallLauncher d = new DemoBallLauncher();
-		d.fire();
 		
 	}
+	
+	public static void initFromWifi(Map data) {
+		GlobalDefinitions.d1 = ((Long) data.get("d1")).intValue();
+		GlobalDefinitions.FWD_CORNER = ((Long) data.get("FWD_CORNER")).intValue() - 1;
+		GlobalDefinitions.DEF_CORNER = ((Long) data.get("DEF_CORNER")).intValue() - 1;
+		GlobalDefinitions.w1 = ((Long) data.get("w1")).intValue();
+		GlobalDefinitions.w2 = ((Long) data.get("w2")).intValue();
+		GlobalDefinitions.bx = ((Long) data.get("bx")).intValue();
+		GlobalDefinitions.by = ((Long) data.get("by")).intValue();
+		GlobalDefinitions.omega = ((Long) data.get("omega")).intValue();
 
+	}
 }
