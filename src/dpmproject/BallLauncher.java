dc4f7d8a1a37dd86793acf650abfbf2ff64ab927
allockicmoi <allockicmoi@gmail.com>
2017-04-02 21:03:03 -0400

    launcher finished

--- src/dpmproject/BallLauncher.java
+++ src/dpmproject/BallLauncher.java
@@ -3,21 +3,27 @@ package dpmproject;
  * 
  */
 import interfacePackages.BallLauncherInterface;
+import lejos.hardware.Sound;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
 /**
  * @author Ali
  *
  */
 public class BallLauncher extends Thread implements BallLauncherInterface {
 
+	
 	@Override
 	public boolean reload() {
-		// TODO Auto-generated method stub
+		pullElasticBack();
+		dropBall();
 		return false;
 	}
 
 	@Override
 	public void fire() {
-		// TODO Auto-generated method stub
+		GlobalDefinitions.SHOOTMOTOR.rotate(-240);
+		Sound.beep();
+		GlobalDefinitions.SHOOTMOTOR.rotate(240);
 		
 	}
 
@@ -27,4 +33,22 @@ public class BallLauncher extends Thread implements BallLauncherInterface {
 		return false;
 	}
 
+	public void pullElasticBack(){
+		GlobalDefinitions.RELOADMOTOR.setSpeed(600);
+		GlobalDefinitions.RELOADMOTOR.rotate(360*21);
+		Sound.beep();
+		GlobalDefinitions.RELOADMOTOR.rotate(-360*21);
+	}
+	
+	public void dropBall(){
+		GlobalDefinitions.SHOOTMOTOR.setSpeed(300);
+		GlobalDefinitions.SHOOTMOTOR.rotate(480);
+		Sound.beep();
+		GlobalDefinitions.SHOOTMOTOR.setSpeed(50);
+
+		GlobalDefinitions.SHOOTMOTOR.rotate(-480);
+		GlobalDefinitions.SHOOTMOTOR.setSpeed(300);
+
+	}
+	
 }
